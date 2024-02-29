e4421be84f2d03d1a73a2a30df0407faf80c90e3
baharma <bagusaditya1899@gmail.com>
2024-03-01 04:21:52 +0800

    what is go

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Printf("hello")
+	// name := "bahar"
+	// age := 99
+
+	// name0, name1, name2 := "daa", "fsa", "dasf"
+
+	pertama, kedua, ketiga := "1", 2, "3ads"
+
+	_, _, _ = pertama, kedua, ketiga
+
+	// fmt.Println("hello : ", name)
+	// fmt.Println("age : ", age)
+
+	// fmt.Println(name0, name1, name2)
+
+	// nama, age, address := "Aditya", 12, "bali"
+
+	// fmt.Printf("halo Nama ku %s , umurku adalah %d, dan aku tinggal di %s", nama, age, address)
+
+	// fmt.Printf("tipe ini adalah %T \n", nama)
+	// fmt.Printf("tipe ini adalah %T \n", age)
 }
