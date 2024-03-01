aa57d4e4fdda4d7f9b729e8bfebba9591f7ac11b
Baharma <bagusaditya1899@gmail.com>
2024-03-01 16:25:33 +0800

    module 2

--- main.go
+++ main.go
@@ -22,4 +22,82 @@ func main() {
 	// fmt.Printf("tipe ini adalah %T \n", nama)
 	// tessfmt.Printf("tipe ini adalah %T \n", age)3ss
 
+	// const age = 2024
+	// if chekc := age - 2001; chekc > 17 {
+	// 	fmt.Println("sim terbuat")
+	// } else {
+	// 	fmt.Println("Sim gagal")
+	// }
+
+	// age := 10
+
+	// if check := 30; check < age {
+	// 	fmt.Println("Tua Ke anjeng")
+	// } else {
+	// 	fmt.Println("haiya")
+	// }
+
+	// score := 4
+
+	// if score > 7 {
+	// 	switch {
+	// 	case score == 8:
+	// 		fmt.Println("nice")
+	// 	case (score < 8) && (score > 5):
+	// 		fmt.Println("good")
+	// 	default:
+	// 		fmt.Println("tolol")
+	// 	}
+	// } else {
+	// 	if score == 5 {
+	// 		fmt.Println("nyerah aja")
+	// 	} else if score == 4 {
+	// 		fmt.Println("kasian")
+	// 	} else {
+	// 		fmt.Println("kamu pasti gak bisa")
+	// 		if score == 0 {
+	// 			fmt.Println("pengangguran")
+	// 		}
+	// 	}
+	// }
+	// i := 0
+	// for {
+	// 	fmt.Println("haha Tolol :", i)
+	// 	i++
+
+	// 	if i == 6 {
+	// 		break
+	// 	}
+	// }
+
+	// for i := 1; i <= 10; i++ {
+	// 	if i%2 == 1 {
+	// 		continue
+	// 	}
+	// 	if i > 8 {
+	// 		break
+	// 	}
+
+	// 	fmt.Println("angka", i)
+	// }
+
+	// for i := 0; i < 5; i++ {
+	// 	for j := i; j < 5; j++ {
+	// 		fmt.Print(j, " ")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	// outerLoop:
+	// 	for i := 0; i < 3; i++ {
+	// 		fmt.Println("perulangan ke -", i+1)
+	// 		for j := 0; j < 3; j++ {
+	// 			if i == 2 {
+	// 				break outerLoop
+	// 			}
+	// 			fmt.Print(j, " ")
+	// 		}
+	// 		fmt.Print("\n")
+
+	// 	}
 }
